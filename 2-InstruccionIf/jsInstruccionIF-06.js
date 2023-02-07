b7e354b9604fb7441646131686d715ce826654a2
Clover <ialderete19@gmail.com>
2023-02-07 03:58:24 -0300

    ejercicios

--- 2-InstruccionIf/jsInstruccionIF-06.js
+++ 2-InstruccionIf/jsInstruccionIF-06.js
@@ -1,8 +1,21 @@
+/* Al ingresar una edad debemos informar si la persona es mayor de edad (mas de 18 años) o adolescente 
+(entre 13 y 17 años) o niño (menor a 13 años).*/
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
+	let edad;
 
+	edad = parseInt(document.getElementById("txtIdEdad").value); 
 
+	if ( edad >= 18){
 
-}//FIN DE LA FUNCIÓN
+		alert("Es mayor de edad");
+	}
+	else {
+		if ( edad >= 13 && edad <= 17){
+		alert("Es adolescente");
+	} 
+	else {
+		alert("Es un niño");
+	}
+	} 
+}
--- 2-InstruccionIf/jsInstruccionIF-10.js
+++ 2-InstruccionIf/jsInstruccionIF-10.js
@@ -1,6 +1,21 @@
+/* Al presionar el Botón, asignar una nota RANDOM al examen y mostrar:"EXCELENTE" para notas igual a 9 o 10,
+"APROBÓ" para notas mayores a 4,
+"Vamos, la proxima se puede" para notas menores a 4*/
 function mostrar()
 {
-	//Genero el número RANDOM entre 1 y 10 
-	alert("ok");
+	let nota;
+	let maximo = 10;
+	let minimo = 1;
 
-}//FIN DE LA FUNCIÓN
+	nota = Math.round(Math.random() * (maximo - minimo) + minimo); 
+
+	if ( nota >= 9){
+		alert("Excelente nota: " + nota);
+	}
+	else if ( nota < 4){
+		alert("Vamos, la proxima se puede. Nota: " + nota);
+	}
+	else{
+		alert("Aprobó. Nota: " + nota);
+	}
+}
